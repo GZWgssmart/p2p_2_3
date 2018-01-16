5f8b543e6450b6b2b0660a72ca823d1149f7a95c
LJYYAnimo <1151757358@qq.com>
2018-01-16 21:03:43 +0800

    修改首页信息pojo 和mapper配置文件

--- src/main/java/com/animo/pojo/Home.java
+++ src/main/java/com/animo/pojo/Home.java
@@ -1,17 +1,33 @@
 package com.animo.pojo;
-
-import java.util.Date;
-
+/**
+ * Created by 7025 on 2017/12/19.
+ * 对应首页
+ */
 public class Home {
     private Integer hid;
 
     private String pic1;
 
+    private String pic2;
+
+    private String pic3;
+
+    private String ewm;
+
+    private String phone;
+
     private String url1;
 
-    private Date createdTime;
+    private String url2;
 
-    private Date updateTime;
+    private String url3;
+
+
+    private Integer status;
+
+    public Home() {
+        super();
+    }
 
     public Integer getHid() {
         return hid;
@@ -29,6 +45,38 @@ public class Home {
         this.pic1 = pic1 == null ? null : pic1.trim();
     }
 
+    public String getPic2() {
+        return pic2;
+    }
+
+    public void setPic2(String pic2) {
+        this.pic2 = pic2 == null ? null : pic2.trim();
+    }
+
+    public String getPic3() {
+        return pic3;
+    }
+
+    public void setPic3(String pic3) {
+        this.pic3 = pic3 == null ? null : pic3.trim();
+    }
+
+    public String getEwm() {
+        return ewm;
+    }
+
+    public void setEwm(String ewm) {
+        this.ewm = ewm == null ? null : ewm.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
     public String getUrl1() {
         return url1;
     }
@@ -37,19 +85,28 @@ public class Home {
         this.url1 = url1 == null ? null : url1.trim();
     }
 
-    public Date getCreatedTime() {
-        return createdTime;
+    public String getUrl2() {
+        return url2;
     }
 
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
+    public void setUrl2(String url2) {
+        this.url2 = url2 == null ? null : url2.trim();
     }
 
-    public Date getUpdateTime() {
-        return updateTime;
+    public String getUrl3() {
+        return url3;
     }
 
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
+    public void setUrl3(String url3) {
+        this.url3 = url3 == null ? null : url3.trim();
+    }
+
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
     }
 }
